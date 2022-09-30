dacc00350699d4625ab614d07ae4c470dad21325
Mark <mutaimark5@gmail.com>
2022-09-30 06:45:14 +0300

    loading state added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,43 +1,47 @@
 //import { ShoppingCart } from '@mui/icons-material';
 import React, { useState, useEffect } from 'react'
-
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import DeleteIcon from '@mui/icons-material/Delete';
 import {auth, database} from '../config/firebaseConfig'
 import IndividualProduct from './IndividualProduct';
 import {Grid} from '@material-ui/core'
+import {ClipLoader} from 'react-spinners/ClipLoader'
 
 
 function Cart() {
 
   const [cartProducts, setCartProducts] = useState([])
+  const [isLoading, setLoading] = useState(false)
 
   useEffect(() => {
     auth.onAuthStateChanged(user =>{
       database.collection('Cart' + user.uid).onSnapshot(snapshot => {
+        setLoading(true) 
         const newCartProduct = snapshot.docs.map((doc) => ({
           ID: doc.id,
-          ...doc.data(),
-        }))
+          ...doc.data(),  
+        })) 
         setCartProducts(newCartProduct)
-      })
-    })
+      })       
+    })    
+    setLoading(false)  
   }, [])
+ 
   
   //console.log(cartProducts);
- 
-
 
   return (
     <>
-    {cartProducts.length > 0 && (
+    {isLoading ? (
+       <div>Loading...</div>
+
+    ) : (
+      <>
+       {cartProducts.length > 0 && (
       <div className="cartcontainer">
         <h1>Cart</h1>
         <div className="products">
         <Grid container justifyContent = 'center' >
           {cartProducts.map((cartProduct) => (
-             <Grid item key={cartProduct.ID} xs = {12} sm = {6} md = {4} lg={3}> 
+             <Grid item key = {cartProduct.ID} xs = {12} sm = {6} md = {4} lg={3}> 
                 <IndividualProduct  cartProduct = {cartProduct} />
               </Grid>
           ))}
@@ -45,8 +49,10 @@ function Cart() {
         </div>
       </div>
     )}
-
-    {cartProducts.length < 1 && (
+      </>
+    )}
+  
+   {cartProducts.length < 1 && (
       <div>No Products.Start adding some</div>
     )}
 
